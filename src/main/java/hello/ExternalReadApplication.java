e667d8aa3bfd7904b9bebf89e21d8656dc0f5598
bluepeng9 <bluepeng9@gmail.com>
2023-06-30 18:39:00 +0900

    feat: Add MyDataSourcePropertiesV1

--- src/main/java/hello/ExternalReadApplication.java
+++ src/main/java/hello/ExternalReadApplication.java
@@ -1,12 +1,15 @@
 package hello;
 
-import hello.config.MyDataSourceValueConfig;
+import hello.config.MyDataSourceConfigV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = "hello.datasource")
-@Import(MyDataSourceValueConfig.class)
+//@Import(MyDataSourceValueConfig.class)
+@Import(MyDataSourceConfigV1.class)
+@ConfigurationPropertiesScan
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
